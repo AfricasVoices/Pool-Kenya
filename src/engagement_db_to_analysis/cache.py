7093639e70bf51e5d6e99d8b4c5ce0bdb7e5928d
IsaackMwenda <mwendaisaac@gmail.com>
2022-05-20 10:19:50 +0300

    add standard pipeline files

---
+++ src/engagement_db_to_analysis/cache.py
@@ -0,0 +1,67 @@
+from os import path
+import json
+
+from core_data_modules.util import IOUtils
+
+from src.common.cache import Cache
+
+
+class AnalysisCache(Cache):
+    def _latest_message_timestamp_path(self, engagement_db_dataset):
+        return f"{self.cache_dir}/last_updated_{engagement_db_dataset}.txt"
+
+    def get_latest_message_timestamp(self, engagement_db_dataset):
+        """
+        Gets the latest seen message.last_updated from cache for the given engagement_db_dataset.
+
+        :param engagement_db_dataset: Engagement db dataset name for this context.
+        :type engagement_db_dataset: str
+        :return: Timestamp for the last updated message in cache, or None if there is no cache yet for this context.
+        :rtype: datetime.datetime | None
+        """
+        return self.get_date_time(engagement_db_dataset)
+
+    def set_latest_message_timestamp(self, engagement_db_dataset, latest_timestamp):
+        """
+        Sets the latest seen message.last_updated in cache for the given engagement_db_dataset context.
+
+        :param engagement_db_dataset: Engagement db dataset name for this context.
+        :type engagement_db_dataset: str
+        :param latest_timestamp: Latest run timestamp.
+        :type latest_timestamp: datetime.datetime
+        """
+        self.set_date_time(engagement_db_dataset, latest_timestamp)
+
+    def set_synced_uuids(self, group_name, participants_uuids):
+        """
+        Sets a set of participants_uuids for the given rapid pro group.
+
+        :param group_name: name of the rapid pro group.
+        :type group_name: str
+        :param participants_uuids: participants uuids to set, for the given rapid pro group.
+        :type participants_uuids: list of participants uuids
+        """
+        export_file_path = path.join(f"{self.cache_dir}/rapid_pro_adverts/{group_name}.jsonl")
+        IOUtils.ensure_dirs_exist_for_file(export_file_path)
+        with open(export_file_path, "w") as f:
+            f.write(json.dumps(participants_uuids))
+
+    def get_synced_uuids(self, group_name):
+        """
+        Gets a set of participants_uuids for the given rapid pro group.
+
+        :param group_name: name of the rapid pro group.
+        :type group_name: str
+        :retun participants_uuids: participants uuids for the given rapid pro group or none if not found.
+        :rtype participants_uuids: list of participants uuids | None
+        """
+
+        previous_export_file_path = path.join(f"{self.cache_dir}/rapid_pro_adverts/{group_name}.jsonl")
+        try:
+            with open(previous_export_file_path) as f:
+                participants_uuids = json.load(f)
+
+        except FileNotFoundError:
+            return []
+
+        return participants_uuids
---
+++ src/pipeline_configuration_spec.py
@@ -0,0 +1,73 @@
+import json
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional, List
+
+from core_data_modules.data_models import CodeScheme
+from core_data_modules.analysis.traffic_analysis import TrafficLabel
+
+from src.common.configuration import (RapidProClientConfiguration, CodaClientConfiguration, UUIDTableClientConfiguration,
+                                      EngagementDatabaseClientConfiguration, OperationsDashboardConfiguration,
+                                      ArchiveConfiguration)
+from src.csv_to_engagement_db.configuration import (CSVSource, CSVDatasetConfiguration)
+
+from src.engagement_db_coda_sync.configuration import (CodaSyncConfiguration, CodaDatasetConfiguration,
+                                                       CodeSchemeConfiguration)
+from src.engagement_db_to_rapid_pro.configuration import (EngagementDBToRapidProConfiguration, DatasetConfiguration,
+                                                          WriteModes, ContactField)
+from src.google_form_to_engagement_db.configuration import (GoogleFormToEngagementDBConfiguration,
+                                                            GoogleFormsClientConfiguration,
+                                                            QuestionConfiguration, GoogleFormSource)
+from src.rapid_pro_to_engagement_db.configuration import (FlowResultConfiguration, UuidFilter,
+                                                          RapidProToEngagementDBConfiguration)
+from src.engagement_db_to_analysis.configuration import (AnalysisDatasetConfiguration, OperatorDatasetConfiguration,
+                                                         DatasetTypes, AgeCategoryConfiguration, AnalysisLocations,
+                                                         CodingConfiguration, GoogleDriveUploadConfiguration,
+                                                         MembershipGroupConfiguration, AnalysisConfiguration)
+from src.facebook_to_engagement_db.configuration import (FacebookSource, FacebookDataset, FacebookSearch)
+from src.telegram_to_engagement_db.configuration import (TelegramGroupSource, TelegramGroupDataset, TelegramGroupSearch)
+
+
+def load_code_scheme(fname):
+    with open(f"code_schemes/{fname}.json") as f:
+        return CodeScheme.from_firebase_map(json.load(f))
+
+
+@dataclass
+class RapidProSource:
+    rapid_pro: RapidProClientConfiguration
+    sync_config: RapidProToEngagementDBConfiguration
+
+
+@dataclass
+class CodaConfiguration:
+    coda: CodaClientConfiguration
+    sync_config: CodaSyncConfiguration
+
+
+@dataclass
+class RapidProTarget:
+    rapid_pro: RapidProClientConfiguration
+    sync_config: EngagementDBToRapidProConfiguration
+
+
+@dataclass
+class PipelineConfiguration:
+    pipeline_name: str
+
+    engagement_database: EngagementDatabaseClientConfiguration
+    uuid_table: UUIDTableClientConfiguration
+    operations_dashboard: OperationsDashboardConfiguration
+    archive_configuration: ArchiveConfiguration
+    project_start_date: datetime = None
+    project_end_date: datetime = None
+    test_participant_uuids: [] = None
+    description: str = None
+    rapid_pro_sources: [RapidProSource] = None
+    facebook_sources: Optional[List[FacebookSource]] = None
+    telegram_group_sources: Optional[List[TelegramGroupSource]] = None
+    csv_sources: Optional[List[CSVSource]] = None
+    google_form_sources: Optional[List[GoogleFormSource]] = None
+    coda_sync: CodaConfiguration = None
+    rapid_pro_target: RapidProTarget = None
+    analysis: AnalysisConfiguration = None
