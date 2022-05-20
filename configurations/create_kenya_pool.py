913ee9e6926052409fb3535dcf67484e2ad8ba97
IsaackMwenda <mwendaisaac@gmail.com>
2022-05-20 18:00:57 +0300

    update configs(users, uuid_file_url, dataset names)

--- configurations/create_kenya_pool.py
+++ configurations/create_kenya_pool.py
@@ -2,22 +2,17 @@ from core_data_modules.cleaners import swahili
 
 from src.pipeline_configuration_spec import *
 
-
-def load_code_scheme(fname):
-    with open(f"code_schemes/{fname}.json") as f:
-        return CodeScheme.from_firebase_map(json.load(f))
-
-
-rapid_pro_uuid_filter = UuidFilter(uuid_file_url="gs://avf-project-datasets/Test/2021/Kenya-Pool/opt_in_uuids.json")
-
+rapid_pro_uuid_filter = UuidFilter(
+    uuid_file_url="gs://avf-project-datasets/2022/POOL-KENYA/initial-pool-kenya-de-identified-uuids.json"
+),
 
 PIPELINE_CONFIGURATION = PipelineConfiguration(
     pipeline_name="Create-Kenya-Pool",
     description="Creates the initial Kenya Pool from demographics responses to COVID19, COVID19-Ke-Urban, "
-                "UNDP-Kenya, UNICEF-KENYA, OXFAM-KENYA, World Vision, and GPSDD.",
+                "UNDP-Kenya, UNICEF-KENYA, OXFAM-KENYA, World Vision, GPSDD and KE polls",
     engagement_database=EngagementDatabaseClientConfiguration(
-        credentials_file_url="gs://avf-credentials/firebase-test.json",
-        database_path="engagement_db_experiments/Kenya"
+        credentials_file_url="gs://avf-credentials/avf-engagement-databases-firebase-credentials-file.json",
+        database_path="engagement_databases/Kenya"
     ),
     uuid_table=UUIDTableClientConfiguration(
         credentials_file_url="gs://avf-credentials/avf-id-infrastructure-firebase-adminsdk-6xps8-b9173f2bfd.json",
@@ -35,15 +30,14 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("covid19_ke_urban_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("covid19_ke_urban_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("covid19_ke_urban_s01_demog", "age", "kenya_pool_age"),
+                    FlowResultConfiguration("covid19_ke_urban_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("covid19_ke_urban_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("covid19_ke_urban_s01_demog", "age", "age"),
 
-                    FlowResultConfiguration("undp_kenya_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("undp_kenya_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("undp_kenya_s01_demog", "age", "kenya_pool_age")
+                    FlowResultConfiguration("undp_kenya_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("undp_kenya_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("undp_kenya_s01_demog", "age", "age")
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -53,11 +47,10 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("covid19_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("covid19_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("covid19_s01_demog", "age", "kenya_pool_age"),
+                    FlowResultConfiguration("covid19_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("covid19_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("covid19_s01_demog", "age", "age"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -67,11 +60,10 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("unicef_covid19_ke_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("unicef_covid19_ke_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("unicef_covid19_ke_s01_demog", "age", "kenya_pool_age"),
+                    FlowResultConfiguration("unicef_covid19_ke_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("unicef_covid19_ke_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("unicef_covid19_ke_s01_demog", "age", "age"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -81,12 +73,11 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("oxfam_wash_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("oxfam_wash_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("oxfam_wash_s01_demog", "age", "kenya_pool_age"),
-                    FlowResultConfiguration("oxfam_wash_s01_demog", "disabled", "kenya_pool_disabled"),
+                    FlowResultConfiguration("oxfam_wash_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("oxfam_wash_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("oxfam_wash_s01_demog", "age", "age"),
+                    FlowResultConfiguration("oxfam_wash_s01_demog", "disabled", "disabled"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -96,11 +87,10 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("worldvision_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("worldvision_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("worldvision_s01_demog", "age", "kenya_pool_age"),
+                    FlowResultConfiguration("worldvision_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("worldvision_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("worldvision_s01_demog", "age", "age"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -110,12 +100,11 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "age", "kenya_pool_age"),
-                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "disabled", "kenya_pool_disabled"),
+                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "age", "age"),
+                    FlowResultConfiguration("gpsdd_kilifi_s01_demog", "disabled", "disabled"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -125,12 +114,11 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "age", "kenya_pool_age"),
-                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "disabled", "kenya_pool_disabled"),
+                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "age", "age"),
+                    FlowResultConfiguration("gpsdd_kiambu_s01_demog", "disabled", "disabled"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
         ),
         RapidProSource(
@@ -140,22 +128,35 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             ),
             sync_config=RapidProToEngagementDBConfiguration(
                 flow_result_configurations=[
-                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "constituency", "kenya_pool_location"),
-                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "gender", "kenya_pool_gender"),
-                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "age", "kenya_pool_age"),
-                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "disabled", "kenya_pool_disabled"),
+                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "constituency", "location"),
+                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "gender", "gender"),
+                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "age", "age"),
+                    FlowResultConfiguration("gpsdd_bungoma_s01_demog", "disabled", "disabled"),
                 ],
-                uuid_filter=rapid_pro_uuid_filter
             )
-        )
+        ),
+        RapidProSource(
+            rapid_pro=RapidProClientConfiguration(
+                domain="textit.com",
+                token_file_url="gs://avf-credentials/ke-vax-textit-token.txt"
+            ),
+            sync_config=RapidProToEngagementDBConfiguration(
+                flow_result_configurations=[
+                    FlowResultConfiguration("ke_constitution_review_poll_demog", "constituency", "location"),
+                    FlowResultConfiguration("ke_constitution_review_poll_demog", "gender", "gender"),
+                    FlowResultConfiguration("ke_constitution_review_poll_demog", "age", "age"),
+                    FlowResultConfiguration("ke_constitution_review_poll_demog", "disabled", "disabled"),
+                ],
+            )
+        ),
     ],
     coda_sync=CodaConfiguration(
         coda=CodaClientConfiguration(credentials_file_url="gs://avf-credentials/coda-production.json"),
         sync_config=CodaSyncConfiguration(
             dataset_configurations=[
                 CodaDatasetConfiguration(
-                    coda_dataset_id="Kenya_Pool_location",
-                    engagement_db_dataset="kenya_pool_location",
+                    coda_dataset_id="Kenya_Pool_location", #TODO Rename as Pool_Kenya_location
+                    engagement_db_dataset="location",
                     code_scheme_configurations=[
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("kenya_constituency"), auto_coder=None),
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("kenya_county"), auto_coder=None)
@@ -164,7 +165,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="Kenya_Pool_gender",
-                    engagement_db_dataset="kenya_pool_gender",
+                    engagement_db_dataset="gender",
                     code_scheme_configurations=[
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("gender"),
                                                 auto_coder=swahili.DemographicCleaner.clean_gender)
@@ -173,7 +174,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="Kenya_Pool_age",
-                    engagement_db_dataset="kenya_pool_age",
+                    engagement_db_dataset="age",
                     code_scheme_configurations=[
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("age"), auto_coder=lambda x:
                                                 str(swahili.DemographicCleaner.clean_age_within_range(x)))
@@ -182,7 +183,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="Kenya_Pool_disabled",
-                    engagement_db_dataset="kenya_pool_disabled",
+                    engagement_db_dataset="disabled",
                     code_scheme_configurations=[
                         CodeSchemeConfiguration(code_scheme=load_code_scheme("disabled"), auto_coder=None)
                     ],
@@ -190,7 +191,12 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 ),
             ],
             ws_correct_dataset_code_scheme=load_code_scheme("kenya_pool_ws_correct_dataset"),
-            default_ws_dataset="kenya_pool_rqa"  # These can be manually archived later.
+            project_users_file_url="gs://avf-project-datasets/2022/POOL-KENYA/pool-kenya-users.json",
+            default_ws_dataset="kenya_pool_old_rqa_datasets"
         )
     ),
+    archive_configuration=ArchiveConfiguration(
+        archive_upload_bucket="gs://pipeline-execution-backup-archive",
+        bucket_dir_path="2022/CREATE-KENYA-POOL/"
+    )
 )
