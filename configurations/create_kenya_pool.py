d0ec307a90135a84c37f08217fda735501a06f81
IsaackMwenda <mwendaisaac@gmail.com>
2022-05-22 10:26:19 +0300

    update names

--- configurations/create_kenya_pool.py
+++ configurations/create_kenya_pool.py
@@ -3,7 +3,7 @@ from core_data_modules.cleaners import swahili
 from src.pipeline_configuration_spec import *
 
 rapid_pro_uuid_filter = UuidFilter(
-    uuid_file_url="gs://avf-project-datasets/2022/POOL-KENYA/initial-pool-kenya-de-identified-uuids.json"
+    uuid_file_url="gs://avf-project-datasets/2022/POOL-KENYA/pool-kenya-initial-de-identified-uuids.json"
 )
 
 PIPELINE_CONFIGURATION = PipelineConfiguration(
@@ -12,7 +12,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 "UNDP-Kenya, UNICEF-KENYA, OXFAM-KENYA, World Vision, GPSDD and KE polls",
     engagement_database=EngagementDatabaseClientConfiguration(
         credentials_file_url="gs://avf-credentials/avf-engagement-databases-firebase-credentials-file.json",
-        database_path="engagement_databases/Kenya"
+        database_path="engagement_databases/KENYA"
     ),
     uuid_table=UUIDTableClientConfiguration(
         credentials_file_url="gs://avf-credentials/avf-id-infrastructure-firebase-adminsdk-6xps8-b9173f2bfd.json",
