06a442d7227aacb9c2e4dbc3744df53f30cbb806
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-06-22 21:47:51 +0300

    survey01 flow configs

--- configurations/AIK_elections_pipeline.py
+++ configurations/AIK_elections_pipeline.py
@@ -54,6 +54,11 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                     FlowResultConfiguration("AIK_survey_demog", "pool_kenya_gender", "gender"),
                     FlowResultConfiguration("AIK_survey_demog", "pool_kenya_location", "location"),
                     FlowResultConfiguration("AIK_survey_demog", "pool_kenya_disabled", "disabled"),
+
+                    # s01q01 -> survey 1 question 1
+                    FlowResultConfiguration("AIK_survey_01_activation", "rqa_aik_elections_s01q01", "aik_elections_s01q01"),
+                    FlowResultConfiguration("AIK_survey_01_activation", "rqa_aik_elections_s01q02", "aik_elections_s01q02"),
+                    FlowResultConfiguration("AIK_survey_01_activation", "rqa_aik_elections_s01q03", "aik_elections_s01q03"),
                 ],
             )
         )
