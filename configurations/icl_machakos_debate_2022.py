c01cd2ce7183361e062f320271debc1c1d147852
IsaackMwenda <mwendaisaac@gmail.com>
2022-06-23 10:16:35 +0300

    update code scheme path

--- configurations/icl_machakos_debate_2022.py
+++ configurations/icl_machakos_debate_2022.py
@@ -177,7 +177,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                 raw_dataset="machakos_womenrep_poll_2022_raw",
                 coding_configs=[
                     CodingConfiguration(
-                        code_scheme=load_code_scheme("rqas/icl/machakos_womenrep_poll_2022"),
+                        code_scheme=load_code_scheme("rqas/icl/machakos_womenrep_poll_2022_selina"),
                         analysis_dataset="machakos_womenrep_poll_2022"
                     )
                 ],
