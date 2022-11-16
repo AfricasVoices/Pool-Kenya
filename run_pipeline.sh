1f4655ae20d3e861ce144f346a1bc79d961bb934
Isaack Mwenda <mwendaisaac@gmail.com>
2022-11-16 17:12:12 +0300

    disable run-engagement-db-to-analysis

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -44,10 +44,9 @@ echo "Starting a new pipeline run with id ${RUN_ID}"
     --incremental-cache-volume "$PIPELINE_NAME-coda-to-engagement-db-cache" \
     "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
 
-
-./docker-run-engagement-db-to-analysis.sh \
-    --incremental-cache-volume "$PIPELINE_NAME-engagement-db-to-analysis-cache" \
-    "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
+#./docker-run-engagement-db-to-analysis.sh \
+    #--incremental-cache-volume "$PIPELINE_NAME-engagement-db-to-analysis-cache" \
+    #"$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_FILE" "$CODE_SCHEMES_DIR" "$DATA_DIR"
 
 ./archive_data_dir.sh "$DATA_DIR" "$ARCHIVE_FILE"
 
