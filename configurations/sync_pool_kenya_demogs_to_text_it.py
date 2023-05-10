e2aa9b2cda587d5547740c90e7c31edb757657e6
Daniel Mwendwa <danielmwendwam@gmail.com>
2023-05-10 12:20:59 +0300

    update database path to rebuild pool kenya

--- configurations/sync_pool_kenya_demogs_to_text_it.py
+++ configurations/sync_pool_kenya_demogs_to_text_it.py
@@ -5,7 +5,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
     description="Syncs the latest demographics data from pool kenya to TextIt",
     engagement_database=EngagementDatabaseClientConfiguration(
         credentials_file_url="gs://avf-credentials/avf-engagement-databases-firebase-credentials-file.json",
-        database_path="engagement_databases/POOL-KENYA"
+        database_path="engagement_databases/POOL-KENYA-REBUILD-MAY-2023"
     ),
     uuid_table=UUIDTableClientConfiguration(
         credentials_file_url="gs://avf-credentials/avf-id-infrastructure-firebase-adminsdk-6xps8-b9173f2bfd.json",
