05ef15f1225d0e1d79a27e138396f33e0151bae9
Daniel Mwendwa <danielmwendwam@gmail.com>
2024-11-15 23:54:45 +0300

    global fund initial config

--- configurations/global_fund_pipeline.py
+++ configurations/global_fund_pipeline.py
@@ -60,6 +60,42 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
         coda=CodaClientConfiguration(credentials_file_url="gs://avf-credentials/coda-production.json"),
         sync_config=CodaSyncConfiguration(
             dataset_configurations=[
+                # CodaDatasetConfiguration(
+                #     coda_dataset_id="Global_Fund_s01e01",
+                #     engagement_db_dataset="gf_s01e01",
+                #     code_scheme_configurations=[
+                #         CodeSchemeConfiguration(code_scheme=load_code_scheme("rqas/global_fund/gf_s01e01"), 
+                #                                 auto_coder=None, coda_code_schemes_count=3)
+                #     ],
+                #     ws_code_match_value="gf_s01e01"
+                # ),
+                # CodaDatasetConfiguration(
+                #     coda_dataset_id="Global_Fund_s01e02",
+                #     engagement_db_dataset="gf_s01e02",
+                #     code_scheme_configurations=[
+                #         CodeSchemeConfiguration(code_scheme=load_code_scheme("rqas/global_fund/gf_s01e02"), 
+                #                                 auto_coder=None, coda_code_schemes_count=3)
+                #     ],
+                #     ws_code_match_value="gf_s01e02"
+                # ),
+                # CodaDatasetConfiguration(
+                #     coda_dataset_id="Global_Fund_s01e03",
+                #     engagement_db_dataset="gf_s01e03",
+                #     code_scheme_configurations=[
+                #         CodeSchemeConfiguration(code_scheme=load_code_scheme("rqas/global_fund/gf_s01e03"), 
+                #                                 auto_coder=None, coda_code_schemes_count=3)
+                #     ],
+                #     ws_code_match_value="gf_s01e03"
+                # ),
+                # CodaDatasetConfiguration(
+                #     coda_dataset_id="Global_Fund_s01e04",
+                #     engagement_db_dataset="gf_s01e04",
+                #     code_scheme_configurations=[
+                #         CodeSchemeConfiguration(code_scheme=load_code_scheme("rqas/global_fund/gf_s01e04"), 
+                #                                 auto_coder=None, coda_code_schemes_count=3)
+                #     ],
+                #     ws_code_match_value="gf_s01e04"
+                # ),
                 CodaDatasetConfiguration(
                     coda_dataset_id="Global_Fund_s01_cop_awareness",
                     engagement_db_dataset="gf_s01_cop_awareness",
@@ -144,6 +180,50 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
             drive_dir="global_fund_s01_analysis_output"
         ),
         dataset_configurations=[
+            # AnalysisDatasetConfiguration(
+            #     engagement_db_datasets=["gf_s01e01"],
+            #     dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
+            #     raw_dataset="gf_s01e01_raw",
+            #     coding_configs=[
+            #         CodingConfiguration(
+            #             code_scheme=load_code_scheme("rqas/global_fund/gf_s01e01"),
+            #             analysis_dataset="s01e01"
+            #         )
+            #     ],
+            # ),
+            # AnalysisDatasetConfiguration(
+            #     engagement_db_datasets=["gf_s01e02"],
+            #     dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
+            #     raw_dataset="gf_s01e02_raw",
+            #     coding_configs=[
+            #         CodingConfiguration(
+            #             code_scheme=load_code_scheme("rqas/global_fund/gf_s01e02"),
+            #             analysis_dataset="s01e02"
+            #         )
+            #     ],
+            # ),
+            # AnalysisDatasetConfiguration(
+            #     engagement_db_datasets=["gf_s01e03"],
+            #     dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
+            #     raw_dataset="gf_s01e03_raw",
+            #     coding_configs=[
+            #         CodingConfiguration(
+            #             code_scheme=load_code_scheme("rqas/global_fund/gf_s01e03"),
+            #             analysis_dataset="s01e03"
+            #         )
+            #     ],
+            # ),
+            # AnalysisDatasetConfiguration(
+            #     engagement_db_datasets=["gf_s01e04"],
+            #     dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
+            #     raw_dataset="gf_s01e04_raw",
+            #     coding_configs=[
+            #         CodingConfiguration(
+            #             code_scheme=load_code_scheme("rqas/global_fund/gf_s01e04"),
+            #             analysis_dataset="s01e04"
+            #         )
+            #     ],
+            # ),
             AnalysisDatasetConfiguration(
                 engagement_db_datasets=["gf_s01_cop_awareness"],
                 dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
